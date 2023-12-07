228e0a29ccf095e9b2899d5cb7dbc69f941f578a
NohanV <149171388+NohanV@users.noreply.github.com>
2023-12-07 11:53:08 +0100

    Update module_traitement.py
    Ajout de la fonction d'extraction du fichier .ics (✅)
    
    Ajout de la fonction traitement de données (❌)
    
    Ajout de la fonction de création de la page web (✅)

--- icalendar_se/module_traitement.py
+++ icalendar_se/module_traitement.py
@@ -1,13 +1,83 @@
-# module_traitement.py
+from datetime import datetime
+
 def extract_data(file_list):
     # Fonction pour extraire les données des fichiers .ics
-    pass
+    data = []
+    for file_path in file_list:
+        with open(file_path, 'r') as file:
+            data.append(file.read())
+    return data
 
 def process_data(data, module_code):
     # Fonction pour traiter les données
-    pass
+    processed_data = []  # Liste stockage des données
+
+    for ics_content in data:
+        lines = ics_content.split('\n')
+        events = []
+        current_event = None
+
+        for line in lines:
+            if line.startswith('BEGIN:VEVENT'): # Commencement de l'evenement
+                current_event = {}
+            elif line.startswith('SUMMARY:'):  # Intitulé de l'événement
+                current_event['summary'] = line.split(':')[-1]
+            elif line.startswith('LOCATION:'):  # Lieu de l'événement
+                current_event['location'] = line.split(':')[-1]
+            elif line.startswith('DESCRIPTION:'):  # Description de l'événement
+                description_parts = line.split('\\n')
+                current_event['group'] = description_parts[2].strip()  # Groupe associé à l'événement
+                current_event['teacher'] = description_parts[3].strip()  # Enseignant associé à l'événement
+            elif line.startswith('DTSTART:'):  # Date de début de l'événement
+                start_time = line.split(':')[-1]
+                # Formatage de l'heure en 'dd-mm-yyyy HH:MM'
+                current_event['start_time'] = datetime.strptime(start_time, '%Y%m%dT%H%M%SZ').strftime('%H:%M')
+                current_event['date'] = datetime.strptime(start_time, '%Y%m%dT%H%M%SZ').strftime('%d%m%y')
+            elif line.startswith('DTEND:'):  # Date de fin de l'événement
+                end_time = line.split(':')[-1]
+                # Conversion du format de date et heure, puis formatage en 'dd-mm-yyyy HH:MM'
+                current_event['end_time'] = datetime.strptime(end_time, '%Y%m%dT%H%M%SZ').strftime('%H:%M')
+            elif line.startswith('END:VEVENT'):  # Fin de l'événement, ajout à la liste
+                events.append(current_event)
+
+        processed_data.extend([event for event in events if module_code in event.get('summary', '')])
+    return processed_data
 
 def generate_html(data, output_dir):
     # Fonction pour générer la page HTML
-    pass
+    html_content = """
+    <html>
+    <body>
+        <table border="1">
+            <tr>
+					 <th>Date</th>                
+                <th>Horaire de début</th>
+                <th>Horaire de fin</th>
+                <th>Nom du cours</th>
+                <th>Salle</th>
+                <th>Groupe</th>
+                <th>Enseignant</th>
+            </tr>
+    """
 
+    for event in data:
+        html_content += f"""
+            <tr>
+                <td>{event['date']}</td>
+                <td>{event['start_time']}</td>
+                <td>{event['end_time']}</td>
+                <td>{event.get('summary', '')}</td>
+                <td>{event.get('location', '')}</td>
+                <td>{event.get('group', '')}</td>
+                <td>{event.get('teacher', '')}</td>
+            </tr>
+        """
+
+    html_content += """
+        </table>
+    </body>
+    </html>
+    """
+
+    with open(output_dir + 'page1.html', 'w') as file:
+        file.write(html_content)
