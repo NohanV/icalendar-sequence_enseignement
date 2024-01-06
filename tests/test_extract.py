bf27b96393cf941c67c073e0d8414ec006316da9
Diryoko <148351646+Diryoko@users.noreply.github.com>
2024-01-06 10:52:18 +0100

    Update test_extract.py

--- tests/test_extract.py
+++ tests/test_extract.py
@@ -8,22 +8,22 @@ Created on Thu Jan  4 12:48:57 2024
 
 import unittest
 from unittest.mock import mock_open, patch
-from module_traitement import extract_data  # Assurez-vous de remplacer "your_module" par le nom réel de votre module
+from module_traitement import extract_data #importation de la fonction tester
 
 class TestExtractData(unittest.TestCase):
 
-    @patch('builtins.open', new_callable=mock_open, read_data='file_content')
-    def test_extract_data_single_file(self, mock_open_file):
+    @patch('builtins.open', new_callable=mock_open, read_data='file_content') #partie réalisé avec la fonction avec l'ouverture et l'extraction des données
+    def test_extract_data_solo_file(self, mock_open_file): #fonction pour un fichier
         file_path = 'test_file.ics'
         file_list = [file_path]
 
         result = extract_data(file_list)
 
-        mock_open_file.assert_called_once_with(file_path, 'r')
+        mock_open_file.assert_called_once_with(file_path, 'r') #partie réalisé avec des assert. Ce qu'on devrait avoir.
         self.assertEqual(result, ['file_content'])
 
     @patch('builtins.open', new_callable=mock_open, read_data='file_content')
-    def test_extract_data_multiple_files(self, mock_open_file):
+    def test_extract_data_multi_files(self, mock_open_file): #fonction pour plusieurs fichier
         file_list = ['file1.ics', 'file2.ics', 'file3.ics']
 
         result = extract_data(file_list)
